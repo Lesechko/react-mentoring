0e08ba8f9c2864e1027de881480c3647f1648d49
Lesechko <alekseylesechko@gmail.com>
2021-10-04 22:37:15 +0300

    add modal for creating movie

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,5 @@ export * from './search';
 export * from './logo';
 export * from './button';
 export * from './sorting';
+export * from './input';
+export * from './modal';
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,38 +1,44 @@
-import {ReactElement} from 'react';
-import {Button, BUTTON_TYPES} from '../common/button';
-import {Logo} from '../common/logo';
-import {Search} from '../common/search';
-import styles from './Header.module.css';
+import { ReactElement, useState } from 'react'
+import { Modal } from '../common'
+import { Button, BUTTON_TYPES } from '../common/button'
+import { Logo } from '../common/logo'
+import { Search } from '../common/search'
+import styles from './Header.module.css'
 
 const Header = (): ReactElement => {
-	const onSearch = (searchTxt: string) => {
-		console.log(searchTxt);
-	};
+  const [formOpen, setFormOpen] = useState(false)
 
-	const onAddClick = () => {
-		alert('Create new Movie');
-	};
+  const onSearch = (searchTxt: string) => {
+    console.log(searchTxt)
+  }
 
-	return (
-		<header className={styles.header}>
-			<div className={styles.wrapper}>
-				<div className={styles.top}>
-					<Logo />
-					<Button type={BUTTON_TYPES.TRANSPARENT} onClick={onAddClick}>
-						+ Add movie
-					</Button>
-				</div>
-				<div className={styles.content}>
-					<div className={styles.title}>Find your movie</div>
-					<Search
-						placeholder="What do you want to watch ?"
-						actionName="Search"
-						onSearch={onSearch}
-					/>
-				</div>
-			</div>
-		</header>
-	);
-};
+  const onAddClick = () => {
+    setFormOpen(true)
+  }
 
-export default Header;
+  return (
+    <>
+      <Modal open={formOpen} onClose={() => setFormOpen(false)} />
+      <header className={styles.header}>
+        <div className={styles.wrapper}>
+          <div className={styles.top}>
+            <Logo />
+            <Button type={BUTTON_TYPES.TRANSPARENT} onClick={onAddClick}>
+              + Add movie
+            </Button>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.title}>Find your movie</div>
+            <Search
+              placeholder="What do you want to watch ?"
+              actionName="Search"
+              onSearch={onSearch}
+            />
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
